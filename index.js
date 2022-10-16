f452dff0433e80c14870e95ea5a7cc7897e960cd
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-16 22:23:25 +0600

    randomly generate outfit

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ app.listen(port,()=>console.log('API server is running'))
 app.get('/outfit',(req,res)=>{
     res.send("This is working")
 })
+app.get("/outfits",(req,res)=>{
+    const tops=['Orange','Blue','White','Yellow'];
+    const jeans=['Black','Navy','Red']
+    const shirts=['Green','Gray','Scarlet']
+    res.json({
+        top:_.sample(tops),
+        jean:_.sample(jeans),
+        shirt:_.sample(shirts)
+    })
+
+})
